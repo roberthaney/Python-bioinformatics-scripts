d12d9eb506ee3e43ac39be76c95dd0d9ce2fb4b5
Rob Haney <robert.a.haney@gmail.com>
2022-06-11 14:16:56 -0400

    Add files via upload
    Added code to store sequences read in dictionary for further use.

--- read_fasta.py
+++ read_fasta.py
@@ -1,3 +1,5 @@
+sequences = {}
+
 def read_fasta_strings(filename):
 	with open(filename) as fhand:
 		return fhand.read().split('>')[1:]
@@ -8,5 +10,12 @@ def read_fasta_entries(filename):
 def read_fasta_sequences(filename):
 	return [[seq[0], seq[2].replace('\n', '')] for seq in read_fasta_entries(filename)]
 
-print read_fasta_sequences('spider_12sp_Hsp75.fa')
+sequence_list = read_fasta_sequences('spider_12sp_Hsp75.fa')
 
+for seq in sequence_list:
+	sequences[seq[0]] = seq[1]
+
+# test
+#for seq in sequences:
+#	print seq 
+#	print sequences[seq]
