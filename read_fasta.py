092eba3244a77219622d9195403b248e131fac4c
Rob Haney <robert.a.haney@gmail.com>
2022-06-13 19:01:56 -0400

    Add files via upload
    Added summary calculations for sequences read.

--- read_fasta.py
+++ read_fasta.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 sequences = {}
 
 def read_fasta_strings(filename):
@@ -11,11 +13,14 @@ def read_fasta_sequences(filename):
 	return [[seq[0], seq[2].replace('\n', '')] for seq in read_fasta_entries(filename)]
 
 sequence_list = read_fasta_sequences('spider_12sp_Hsp75.fa')
+seq_lengths = []
 
 for seq in sequence_list:
 	sequences[seq[0]] = seq[1]
+	seq_lengths.append(len(sequences[seq[0]]))
 
-# test
-#for seq in sequences:
-#	print seq 
-#	print sequences[seq]
+print "\nSummary of sequence set values:"
+print "Longest sequence:", max(seq_lengths)
+print "Shortest sequence:", min(seq_lengths)
+print "Mean sequence length:", np.mean(seq_lengths)
+print "Median sequence length:", np.median(seq_lengths)
