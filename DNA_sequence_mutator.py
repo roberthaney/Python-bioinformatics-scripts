0841a8810d4cd12c013124560c27582e57a13674
Rob Haney <robert.a.haney@gmail.com>
2022-06-16 13:19:57 -0400

    Add files via upload
    Modified function to allow for choice of number of mutations to induce in sequence.

--- DNA_sequence_mutator.py
+++ DNA_sequence_mutator.py
@@ -5,18 +5,22 @@ def validate_sequence(sequence):
 	seq = sequence.upper()
 	return len(seq) == (seq.count("A") + seq.count("C") + seq.count("G") + seq.count("T"))
 
-def replace_random_base(DNA_seq):
+def replace_random_bases(DNA_seq, num):
 	"""Return a DNA sequence with a randomly selected position replaced by a randomly chosen base"""
-	if validate_sequence(DNA_seq):
-		position = randint(0, len(DNA_seq) - 1)
-		# replace base in position with empty string to make sure same base not chosen
-		bases = 'TCAG'.replace(DNA_seq[position], '')
-		# show mutated base in lowercase
-		return DNA_seq[0:position] + bases[randint(0,2)].lower() + DNA_seq[position + 1:]
+	DNA = DNA_seq
+	if validate_sequence(DNA):
+		for i in range(num):
+			position = randint(0, len(DNA) - 1)
+			# replace base in position with empty string to make sure same base not chosen
+			bases = 'TCAG'.replace(DNA[position], '')
+			# show mutated base in lowercase
+			DNA = DNA[0:position] + bases[randint(0,2)].lower() + DNA[position + 1:]
+		return DNA
 	else:
 		return "Sequence does not appear to be DNA"
+
 # test
-print replace_random_base('ATCGGCGATGCAGTACGATACTA')
-print replace_random_base('ATCGGCTACCCGGGTTAATGACA')
-print replace_random_base('ATCGGCTACCCGGXTTAATGACA')
+print replace_random_bases('ATCGGCGATGCAGTACGATACTA', 10)
+print replace_random_bases('ATCGGCTACCCGGGTTAATGACA', 1)
+print replace_random_bases('ATCGGCTACCCGGXTTAATGACA', 1)
 
