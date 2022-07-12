0b3def775830728b432e19e11391d46c7b2b005f
Rob Haney <robert.a.haney@gmail.com>
2022-07-12 14:51:50 -0400

    Add files via upload
    Added docstring and main function.

--- DNA_nucl_counts.py
+++ DNA_nucl_counts.py
@@ -1,6 +1,13 @@
 
 def nucl_count(DNA):
+	"""returns counts of nucleotides in DNA sequence as list"""
 	return [DNA.count("A"), DNA.count("C"), DNA.count("G"), DNA.count("T")]
 
+def main():
+	print(nucl_count("ATCGATCGATCG"))
 
-print(nucl_count("ATCGATCGATCG"))
+if __name__=='__main__':
+	main()
+
+
+
