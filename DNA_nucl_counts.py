9060e940680babfa3ebd23624e1cb08f385174c5
Rob Haney <robert.a.haney@gmail.com>
2022-07-14 11:57:44 -0400

    Add files via upload
    Added parser for command line arguments.

--- DNA_nucl_counts.py
+++ DNA_nucl_counts.py
@@ -1,10 +1,18 @@
+import argparse
+
+def retrieve_args():
+	"""retrieve and parse command line arguments"""
+	arg_parser = argparse.ArgumentParser()
+	arg_parser.add_argument('dna_seq', metavar='DNA', help='Input DNA sequence')
+	return arg_parser.parse_args()
 
 def nucl_count(DNA):
 	"""returns counts of nucleotides in DNA sequence as list"""
 	return [DNA.count("A"), DNA.count("C"), DNA.count("G"), DNA.count("T")]
 
 def main():
-	print(nucl_count("ATCGATCGATCG"))
+	args = retrieve_args()
+	print(nucl_count(args.dna_seq))
 
 if __name__=='__main__':
 	main()
