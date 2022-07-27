029722256b5b1a434c88bf0afa924b925cad7836
Rob Haney <robert.a.haney@gmail.com>
2022-07-27 10:38:19 -0400

    Add files via upload
    Added code to average counts across replicates for tissues.

--- calculate_tau.py
+++ calculate_tau.py
@@ -1,6 +1,8 @@
+import numpy as np
+
 fhand = open("BW_rnd3.all.maker.ucode_longest.transcripts_Salmon_edgeR_numreads_matrix.txt")
 
-gene_counts = {}
+gene_averaged_counts = {}
 normalized_counts = {}
 tau = {}
 
@@ -8,12 +10,31 @@ for line in fhand:
 	if line.startswith("Transcript"):
 		continue
 	temp = line.split()
-	gene_counts[temp[0]] = temp[1:]
+	gene_averaged_counts[temp[0]] = {}
+	gene_averaged_counts[temp[0]]["Agg"] = np.mean([float(x) for x in temp[1:3]])
+	gene_averaged_counts[temp[0]]["Ceph"] = np.mean([float(x) for x in temp[3:5]])
+	gene_averaged_counts[temp[0]]["Flag"] = np.mean([float(x) for x in temp[5:7]])
+	gene_averaged_counts[temp[0]]["Major"] = np.mean([float(x) for x in temp[7:9]])
+	gene_averaged_counts[temp[0]]["Minor"] = np.mean([float(x) for x in temp[9:11]])
+	gene_averaged_counts[temp[0]]["Ovary"] = np.mean([float(x) for x in temp[11:13]])
+	gene_averaged_counts[temp[0]]["Tub"] = np.mean([float(x) for x in temp[13:15]])
+	gene_averaged_counts[temp[0]]["Venom"] = np.mean([float(x) for x in temp[15:18]])
 
-for gene in gene_counts:
-	max_value = max(gene_counts[gene])
+
+print(gene_averaged_counts["Lhe_031888-RA"]["Agg"])
+print(gene_averaged_counts["Lhe_031888-RA"]["Ceph"])
+print(gene_averaged_counts["Lhe_031888-RA"]["Flag"])
+print(gene_averaged_counts["Lhe_031888-RA"]["Major"])
+print(gene_averaged_counts["Lhe_031888-RA"]["Minor"])
+print(gene_averaged_counts["Lhe_031888-RA"]["Ovary"])
+print(gene_averaged_counts["Lhe_031888-RA"]["Tub"])
+print(gene_averaged_counts["Lhe_031888-RA"]["Venom"])
+
+'''
+for gene in gene_averaged_counts:
+	max_value = max(gene_averaged_counts[gene])
 	normalized_counts[gene] = [] 
-	for count in gene_counts[gene]:
+	for count in gene_averaged_counts[gene]:
 		normalized_counts[gene].append(count/max_value)
 
 for gene in normalized_counts:
@@ -22,5 +43,5 @@ for gene in normalized_counts:
 	for count in normalized_counts[gene]:
 		count_sum += 1 1- count
 	normalized_count_sum = count_sum/len(normalized_counts[gene])
-
+'''
 
