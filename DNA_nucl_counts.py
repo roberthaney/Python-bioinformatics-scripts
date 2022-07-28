f1fcf05339724f802bac0e49422651512e526e46
Rob Haney <robert.a.haney@gmail.com>
2022-07-28 14:18:38 -0400

    Add files via upload
    Added class for arguments.

--- DNA_nucl_counts.py
+++ DNA_nucl_counts.py
@@ -1,10 +1,16 @@
 import argparse
+from typing import NamedTuple
 
-def retrieve_args():
+class Args(NamedTuple):
+	"""structure to store command line arguments"""
+	dna_seq: str
+
+def retrieve_args() -> Args:
 	"""retrieve and parse command line arguments"""
 	arg_parser = argparse.ArgumentParser()
 	arg_parser.add_argument('dna_seq', metavar='DNA', help='Input DNA sequence')
-	return arg_parser.parse_args()
+	args = arg_parser.parse_args()
+	return Args(args.dna_seq)
 
 def nucl_count(DNA):
 	"""returns counts of nucleotides in DNA sequence as list"""
